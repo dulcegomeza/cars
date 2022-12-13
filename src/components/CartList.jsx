734039058a0024fa66e7bf1ad9a00a65bec9ebbc
dulcegomeza <athaladulceedithgomezarellano@gmail.com>
2022-12-13 13:56:04 -0600

    tooltip

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,47 +1,49 @@
-import  {useContext} from "react";
+import { useContext } from "react";
 import ModelContext from "../context/ModelContext";
-import { NavLink } from "react-router-dom";
+import { FaTrashAlt } from "react-icons/fa";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
 
 const CartList = () => {
-    const { cart, deleteModelToCart } = useContext (ModelContext)
+  const { cart, deleteModelToCart } = useContext(ModelContext)
 
-    return (
-        <>
-        {
-               cart?.map( (model) =>(
-                <div className="card rounded-3 mb-4" key={model.uid}>
-        
-                  <div id="div_cart" className="card-body">
-                    <div className="row d-flex justify-content-between align-items-center">
-                      <div id="img_cart" className="col-md-3 col-lg-2 col-sm-8">
-                        <img  src={model.imgs[0]} className="img-fluid" alt={model.model} />
-                      </div>
-        
-                      <div id="div_productCartText" className="col-md-4 col-lg-4 col-sm-10">
-                        <p>{model.model}</p>
-                        <p><span className="text-muted">{model.description}</span></p>
-                      </div>
-                      <div id="div_productCartPrice" className="col-md-3 col-lg-3 col-sm-10">
-                        <p>$ {model.price} mxn</p>
-                      </div>
-        
-                      <div className="col-md-2 col-lg-3 col-sm-12">
-                        <NavLink id="btn_seeProductCart" className="btn btn-blanck-form w-100 mb-2" to={`/cars/${model.uid}`}> 
-                          Detalles
-                        </NavLink>
-                        <button id="btn_deleteProductCart" onClick={()=> deleteModelToCart(model.uid)} className="btn btn-danger w-100">
-                          Quitar
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+  return (
+    <>
+      {
+        cart?.map((model) => (
+          <div className="mb-4" key={model.uid}>
+
+            <div >
+              <div className="row d-flex justify-content-between align-items-center">
+                <div id="img_cart" className="col-md-3 col-lg-2 col-sm-8">
+                  <img src={model.imgs[0]} className="img-fluid" alt={model.model} />
                 </div>
-        
-                ))
 
-        }
-        </>
-    )
+                <div className="col-md-4 col-lg-4 col-sm-10">
+                  <p>{model.model}</p>
+                  <p><span className="text-muted">{model.description}</span></p>
+                </div>
+                <div className="col-md-3 col-lg-3 col-sm-10">
+                  <p>$ {model.price} mxn</p>
+                </div>
+                <div className="col-md-2  col-sm-12">
+                  <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Quitar</Tooltip>}>
+                    <span className="d-inline-block">
+                      <button onClick={() => deleteModelToCart(model.uid)} className="btn btn-danger w-100">
+                        <FaTrashAlt size={20}></FaTrashAlt>
+                      </button>
+                    </span>
+                  </OverlayTrigger>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+
+      }
+    </>
+  )
 }
 
 export default CartList;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import  {useState, useContext, useEffect} from "react";
+import { useState, useContext, useEffect } from "react";
 import ModelContext from "../context/ModelContext";
 import CartList from "../components/CartList";
 import PaypalCheckoutButton from "../components/PaypalCheckoutButton";
@@ -6,72 +6,68 @@ import PaypalCheckoutButton from "../components/PaypalCheckoutButton";
 
 
 function Cart() {
-    const { cart } = useContext ( ModelContext )
+  const { cart } = useContext(ModelContext)
 
-    const [total, setTotal] = useState(0);
-  
-    useEffect(() => {
-        setTotal( cart.reduce((acumulador, model)=> 
-            acumulador + model.price,0
-        ))
-      }, [cart])
+  const [total, setTotal] = useState(0);
 
-  
+  useEffect(() => {
+    setTotal(cart.reduce((acumulador, model) =>
+      acumulador + model.price, 0
+    ))
+  }, [cart])
+
+
   return (
-  <>
-   <section className="h-100  animate__animated animate__fadeIn" >
+    <>
+      <section className="h-100  animate__animated animate__fadeIn" >
         <div className="container py-2 h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col">
-              <div className="card">
-                <div className="card-body">
-                  <div className="row">
+
+              <div className="row">
+
+                {cart.length > 0 ? (
+                  <>
                     <div className="col-lg-7">
-                    {cart.length > 0 ? (
-                      <>
-                        <CartList />
-                      </>
-                    ) : (
-                      <section className="text-center">
-                        <h3>¡No tienes productos aun en tu carrito!</h3>
-                      </section>
-                    )
-                    }
+                      <div className="card mb-2">
+                        <div className="card-body">
+                          <CartList />
+                        </div>
+                      </div>
                     </div>
                     <div className="col-lg-5 mx-auto mt-auto mb-auto">
-                      {cart.length > 0 ? (
-                          <>
-                              <section className="row mb-5">
-                                <article className="col-md-12 text-center">
-                                  <h2>Total a pagar: ${total} mxn </h2>
-                                </article>
-                              </section>
-                              <section className="row">
-                                <article className="col">
-                                  <PaypalCheckoutButton  currency={"MXN"}
-                                    amount={total}
-                                    showSpinner={false}/>
-                                </article>
-                              </section>
-                          </>
-                        ) : (                            
-                              <section className="text-center">
-                                  <article>
-                                    <h2> ${total} mxn </h2>
-                                  </article>
-                              </section>)}
+                      <section className="row mb-5">
+                        <article className="col-md-12 text-center">
+                          <h2>Total a pagar: ${total} mxn </h2>
+                        </article>
+                      </section>
+                      <section className="row">
+                        <article className="col">
+                          <PaypalCheckoutButton currency={"MXN"}
+                            amount={total}
+                            showSpinner={false} />
+                        </article>
+                      </section>
                     </div>
-
+                  </>
+                ) : (
+                  <div className="text-center mt-5">
+                    <h3>¡No tienes productos aun en tu carrito!</h3>
                   </div>
-                  
-                </div>
+                )}
+
+
+
+
               </div>
+
+
             </div>
           </div>
-        </div> 
+        </div>
       </section>
 
-  </>
+    </>
 
   )
 }
